9c3c18784ee01648760542037624869a65664db7
kefir90 <148564871+kefir90@users.noreply.github.com>
2023-10-20 18:06:46 +0300

    Add files via upload

---
+++ src/Main.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kefir\RustLight;
+
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+
+class FirstPlugin extends PluginBase implements Listener {
+    public function onEnable() {
+        $this->getServer()->getLogger()->info("§aНаш плагин запустился");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onDisable() {
+        $this->getServer()->getLogger()->info("§cНаш плагин выключился");
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
+        $commandName = $command->getName();
+        $nickName = $sender->getName();
+        if ($commandName == "craft") {
+            $sender->sendMessage("Крафт");
+        }
+        return false;
+    }
+
+    public function onJoin(PlayerJoinEvent $event) {
+        $player = $event->getPlayer();
+        $nickName = $player->getName();
+        $event->setJoinMessage("§aИгрок §b$nickName §aзашел на сервер! §6Поприветствуем!");
+        $player->sendMessage("§eТы зашел на сервер! §b$nickName");
+    }
+
+    public function onMove(PlayerMoveEvent $event) {
+        $player = $event->getPlayer();
+        $nickName = $player->getName();
+        $x = $player->getFloorX();
+        $y = $player->getFloorY();
+        $z = $player->getFloorZ();
+        $player->sendTip("$nickName: x: $x, y: $y, z: $z");
+    }
+}
