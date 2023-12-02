1b78ba5a915ef360873af0ba766232eb551bfb80
kefir90 <148564871+kefir90@users.noreply.github.com>
2023-12-02 17:00:20 +0300

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -4,75 +4,50 @@ declare(strict_types=1);
 
 namespace Kefir\RustLight;
 
-use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
+use pocketmine\command\{Command, CommandSender};
+use pocketmine\player\Player
+use pocketmine\Server
+use pocketmine\inventory\Inventory
 
-use pocketmine\command\CommandSender;
-use pocketmine\command\Command;
+use Frago9876543210\EasyForms\elements\Button
+use Frago9876543210\EasyForms\forms\MenuForm
 
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\Player;
-use pocketmine\Server;
-use jojoe77777\FormAPI\SimpleForm;
-
-class FirstPlugin extends PluginBase implements Listener {
-
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
-        $commandName = $command->getName();
-        $nickName = $sender->getName();
-        if (strtolower($commandName) === "craft") {
-            $sender->sendMessage("Крафт");
-            $this->openCustomMenu($sender);
-            return true;
-        }
-        return false;
-    }
-
-    public function onEnable(): void {
-        $this->getServer()->getLogger()->info("§cRustLightPlugin is starting");
-        $this->getServer()->getLogger()->info("§cAvoid ку кста");
-    }
-
-    public function onDisable(): void {
-        $this->getServer()->getLogger()->info("§cRustLightPlugin is shutdown");
-    }
-
-    public function onJoin(PlayerJoinEvent $event) {
-        $player = $event->getPlayer();
-        $nickName = $player->getName();
-        $event->setJoinMessage("§aИгрок §b$nickName §aзашел на сервер!");
-        $player->sendMessage("§eТы зашел на сервер! §b$nickName");
-    }
-
-    private function openCustomMenu(Player $player) {
-        $form = new SimpleForm(function (Player $player, $data) {
-            if ($data === null) return;
-            // Обработка выбора из меню
-            switch ($data) {
-                case 0:
-                    $player->sendMessage("Выбрана опция 0");
-                    break;
-                case 1:
-                    $player->sendMessage("Выбрана опция 1");
-                    break;
-                // и так далее
-            }
-        });
-        $form->setTitle("Крафт");
-        $form->setContent("Выберите предмет");
-        $form->addButton("Киянка");
-        $form->addButton("План постройки");
-        $form->addButton("Каменный топор");
-        $form->addButton("Каменная кирка");
-        $form->addButton("Камень");
-        $form->addButton("Верстак первого уровня");
-        $form->addButton("Деревянное копье");
-        $form->addButton("Каменное копье");
-        $form->addButton("Печь");
-        $form->addButton("Замок");
-        $form->addButton("Деревянная дверь");
-        $form->addButton("Кодовый замок");
-        // и так далее
-        $form->sendToPlayer($player);
-    }
+final class Main extends PluginBase
+{
+	function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool
+	{
+		if(!$sender instanceof Player) { $sender->sendMessage("Испольуйте только в игре"); return true; }
+		if(count($args) !== 0) { $sender->sendMessage("Испольуйте: /cr"); return true; }
+		$sender->sendForm(new MenuForm("Крафт")
+						[
+							new Button("План постройки"),
+							new Button("Киянка"),
+							new Button("Деревянная дверь"),
+							new Button("Металлическая дверь"),
+							new Button("Бронированная дверь"),
+							new Button("Дверной замок"),
+							new Button("Кодовый замок"),
+							new Button("Деревянное копье"),
+							new Button("Каменное копье"),
+							new Button("Каменный топор"),
+							new Button("Каменная кирка"),
+							new Button("Металлический топор"),
+							new Button("Металлическая кирка"),
+							new Button("Самодельный топор"),
+							new Button("Самодельный ледоруб"),
+						],
+						function(Player $sender, Button $button) : void
+						{
+							if($button-> getValue() === 0)
+							{
+								$sender->getInventory()->addItem(Item::get(my:building_plan));
+							}
+							if($button-> getValue() === 1)
+							{
+								$sender->getInventory()->addItem(Item::get(my:kuyanka));
+							}
+		return true;	}}};			
+	}
 }
+?>
